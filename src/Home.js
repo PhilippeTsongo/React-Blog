7988120ede2e021642caedc5617b2cca61a30804
philippetsongo <philippetsongo90@gmail.com>
2024-01-06 18:16:48 +0200

    Passing props in the stateless function component

--- src/Home.js
+++ src/Home.js
@@ -8,13 +8,10 @@ const Home = () => {
         {title: 'Traffic jams', body: 'This is actually caussed by drivers who don\'t respect the code', author: 'Tsongo', id: 2},
         {title: 'The last day', body: 'On the last day of our training at this amazing and ....', author: 'Phil', id: 3},
     ]);
-
-    const title = 'All  articles';    
-
    
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title={title} />
+            <BlogList blogs={blogs} title="All Articles" />
         </div>
      );
 }
