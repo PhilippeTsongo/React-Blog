6254208d912153b637dd74121e317ed706cac7f3
philippetsongo <philippetsongo90@gmail.com>
2024-01-15 09:22:18 +0200

    review of the reusable useFetch component as well as BlogDeteail component

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const { title, setTitle } =  useState('');
+    const { body, setBody } =  useState('');
+    // const { author, setAuthor } =  useState('');
+
     return (  
         <div className="create">
             <h2>Add a New Article</h2>
+
+            <form>
+                <label>Article Tile</label>
+                <input
+                    type="text"
+                    value={ title }
+                    onchange={ (e) => setTitle(e.target.value)}
+                    required
+                >
+                </input>
+                
+                <label>Article Body</label>
+                <textarea
+                    required
+                    value={ body }
+                    onchange={ (e) => setBody(e.target.value)}
+                >
+                </textarea>
+                
+                <label>Article Author</label>
+                <select>
+                    <option>Philippe</option>
+                    <option>Tsongo</option>
+                </select>
+
+                <button>Add Article</button>
+                <p>{ title }</p>
+                <p>{ body }</p>
+            </form>
         </div>
     );
 }
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,35 +1,33 @@
-
 import { useState, useEffect } from "react";
 
-const useFetch = ( (url) => {
-
+const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect( () => {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
 
-        setTimeout(() => {
-            fetch(url)
-                .then((response) => {
-                    if(!response.ok){
-                        throw Error('Failed to fetch data from the resouce');
-                    }
-                    return response.json();
-                })
-                .then((data) => {
-                    setData(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch((err) =>{
-                    setError(err.message);
-                    setIsPending(false);
-                })
-        }, 1000)
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data from the resource');
+                }
+                const jsonData = await response.json();
+
+                setData(jsonData);
+                setIsPending(false);
+                setError(null);
+            } catch (err) {
+                setError(err.message);
+                setIsPending(false);
+            }
+        };
+
+        setTimeout(fetchData, 1000);
     }, [url]);
-    
-    return { data, setData, isPending, error }
-})
 
-export default useFetch
+    return { data, isPending, error };
+};
+
+export default useFetch;
